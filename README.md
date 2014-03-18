311d8cedcbc05320d230440ffe7b8af06bc443b7
Nicolas BERTRAND <bertrand.nicolas.90@gmail.com>
2014-03-18 10:14:06 +0100

    New install script for zsh

--- README.md
+++ README.md
@@ -2,4 +2,9 @@
 ========
 
 This is my emacs configuration. Nothing really fancy going on here ;]
+
 Using Cask + pallet + use-package.
+
+Clone this repo then use the following :
+
+	$ ./install.sh
---
+++ install.sh
@@ -0,0 +1,29 @@
+#/bin/zsh
+
+set -e
+
+installed=$(which cask)
+if ${installed}; then
+    echo "Already installed."
+    exit 0
+fi
+
+get_absolute_path() {
+	pushd `dirname $0` > /dev/null
+	pwd -P
+	popd > /dev/null
+}
+
+curl -fsSkL https://raw.github.com/cask/cask/master/go | python
+
+if grep --quiet cask ~/.zshenv; then
+    echo "typeset -U path" > ~/.zshenv
+    echo "path+=(~/.cask/bin)" >> ~/.zshenv
+    source ~/.zshenv
+fi
+
+emacsdir=$(get_absolute_path)
+
+cd ${emacsdir}
+git pull --rebase
+cask install
