19b32bde5335339a417327ce8577e389983a20c2
Nicolas BERTRAND <bertrand.nicolas.90@gmail.com>
2014-08-18 15:17:38 +0200

    Fix install.sh

--- install.sh
+++ install.sh
@@ -11,7 +11,8 @@ get_absolute_path() {
 if [ ! -d ~/.cask/ ]; then
 	curl -fsSkL https://raw.github.com/cask/cask/master/go | python
 
-	if test ! -f ~/.zshenv || grep --quiet cask ~/.zshenv; then
+    echo "" >> ~/.zshenv
+	if grep --quiet cask ~/.zshenv; then
 		echo "path+=(~/.cask/bin)" >> ~/.zshenv
 	fi
 	typeset -gxU path
